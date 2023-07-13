a7ab8c0c2e75ef147437188ca8a188d2068bf58b
patotec <ewaenpatrick5@gmail.com>
2023-07-13 09:40:17 +0100

    first

--- user/views.py
+++ user/views.py
@@ -73,19 +73,22 @@ def withdraw(request):
         wallet = request.POST.get('wal')
         amount = request.POST.get('amount')
         coin = request.POST.get('coin')
-        user = User.objects.get(username=request.user)
-        qs = Withdraw(wallet=wallet,amount=amount,user=user)
-        qs.save()
-        # randompin = ''.join(random.choice(string.digits) for _ in range(4))
-        # create = Pin.objects.create(user=user, pin=randompin, email=request.user.email)
-        # msg = EmailMessage(
-        # 'Pin request',
-        # create.user.username + " Has requested for pin NO. " + create.pin + " , check your dashboard for more info",
-        # settings.DEFAULT_FROM_EMAIL,
-        # ['rworkah@hotmail.com'],
-        # )
-        # msg.send()
-        return render(request, 'acc/suc1.html')
+        if float(amount) > float(request.user.total_withdrawal):
+            messages.error(request, 'Insufficient Funds')
+        else:
+            user = User.objects.get(username=request.user)
+            qs = Withdraw(wallet=wallet,amount=amount,user=user,coin=coin)
+            qs.save()
+            # randompin = ''.join(random.choice(string.digits) for _ in range(4))
+            # create = Pin.objects.create(user=user, pin=randompin, email=request.user.email)
+            # msg = EmailMessage(
+            # 'Pin request',
+            # create.user.username + " Has requested for pin NO. " + create.pin + " , check your dashboard for more info",
+            # settings.DEFAULT_FROM_EMAIL,
+            # ['rworkah@hotmail.com'],
+            # )
+            # msg.send()
+            return render(request, 'acc/suc1.html')
     return render(request, 'acc/with.html')
 
 
